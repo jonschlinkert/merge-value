83e043ba051bda5616b969615cb30beabe6c0393
jonschlinkert <jon.schlinkert@sellside.com>
2017-07-20 17:18:37 -0400

    use assert

--- test.js
+++ test.js
@@ -1,51 +1,50 @@
 'use strict';
 
-/* deps: mocha */
+require('mocha');
 var assert = require('assert');
-var should = require('should');
 var merge = require('./');
 
-describe('merge value', function () {
-  it('should merge a value:', function () {
+describe('merge value', function() {
+  it('should merge a value:', function() {
     var obj = {};
-    merge(obj, {a: 'b'})
-    obj.should.eql({a: 'b'});
+    merge(obj, {a: 'b'});
+    assert.deepEqual(obj, {a: 'b'});
   });
 
-  it('should merge a nested value:', function () {
+  it('should merge a nested value:', function() {
     var obj = {};
-    merge(obj, 'foo', {a: 'b'})
-    obj.foo.should.eql({a: 'b'});
+    merge(obj, 'foo', {a: 'b'});
+    assert.deepEqual(obj.foo, {a: 'b'});
   });
 
-  it('should work for key/value pairs:', function () {
+  it('should work for key/value pairs:', function() {
     var obj = {};
     merge(obj, 'foo', 'b');
-    obj.foo.should.equal('b');
+    assert.equal(obj.foo, 'b');
   });
 
-  it('should merge an existing value:', function () {
+  it('should merge an existing value:', function() {
     var obj = {foo: {a: 'b'}};
-    merge(obj, 'foo', {c: 'd'})
-    obj.foo.should.eql({a: 'b', c: 'd'});
+    merge(obj, 'foo', {c: 'd'});
+    assert.deepEqual(obj.foo, {a: 'b', c: 'd'});
   });
 
-  it('should deeply merge an existing value:', function () {
+  it('should deeply merge an existing value:', function() {
     var obj = {foo: {bar: {baz: {a: 'b'}}}};
     merge(obj, 'foo', {bar: {baz: {c: 'd'}}});
-    obj.foo.bar.baz.should.eql({a: 'b', c: 'd'});
+    assert.deepEqual(obj.foo.bar.baz, {a: 'b', c: 'd'});
   });
 
-  it('should merge a deeply nested value:', function () {
+  it('should merge a deeply nested value:', function() {
     var obj = {};
     merge(obj, 'a.b.c', {one: 'two'});
     merge(obj, 'a.b.c', {three: 'four'});
-    obj.a.b.c.should.eql({one: 'two', three: 'four'});
+    assert.deepEqual(obj.a.b.c, {one: 'two', three: 'four'});
   });
 
-  it('should throw an error when invalid args are passed:', function () {
-    (function () {
+  it('should throw an error when invalid args are passed:', function() {
+    assert.throws(function() {
       merge();
-    }).should.throw('expected the first argument to be an object.');
+    });
   });
 });
